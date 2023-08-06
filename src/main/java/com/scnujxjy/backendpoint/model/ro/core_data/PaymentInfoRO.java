1d0b89c51caed57dbcb91e2cfeb0282c4e00f0f1
liweitang <levitang@126.com>
2023-08-06 23:06:19 +0800

    update:支付信息实体类

---
+++ src/main/java/com/scnujxjy/backendpoint/model/ro/core_data/PaymentInfoRO.java
@@ -0,0 +1,78 @@
+package com.scnujxjy.backendpoint.model.ro.core_data;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Builder
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Accessors(chain = true)
+public class PaymentInfoRO {
+    /**
+     * 主键id
+     */
+    private Long id;
+
+    /**
+     * 学号
+     */
+    private String studentNumber;
+
+    /**
+     * 准考证号
+     */
+    private String admissionNumber;
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 身份证号码
+     */
+    private String idCardNumber;
+
+    /**
+     * 缴费日期
+     */
+    private Date paymentDate;
+
+    /**
+     * 缴费类别
+     */
+    private String paymentCategory;
+
+    /**
+     * 学年
+     */
+    private String academicYear;
+
+    /**
+     * 缴费类型
+     */
+    private String paymentType;
+
+    /**
+     * 金额
+     */
+    private BigDecimal amount;
+
+    /**
+     * 是否已缴费
+     */
+    private String isPaid;
+
+    /**
+     * 缴费方式
+     */
+    private String paymentMethod;
+
+}
