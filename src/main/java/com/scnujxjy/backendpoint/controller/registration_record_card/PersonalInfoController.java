3db08100b24bd5176a8e6db9a2037bee056ff60c
liweitang <levitang@126.com>
2023-08-07 21:05:35 +0800

    update:个人基本信息CURD完成

--- src/main/java/com/scnujxjy/backendpoint/controller/registration_record_card/PersonalInfoController.java
+++ src/main/java/com/scnujxjy/backendpoint/controller/registration_record_card/PersonalInfoController.java
@@ -1,9 +1,19 @@
 package com.scnujxjy.backendpoint.controller.registration_record_card;
 
 
+import cn.dev33.satoken.util.SaResult;
+import com.scnujxjy.backendpoint.model.ro.PageRO;
+import com.scnujxjy.backendpoint.model.ro.registration_record_card.PersonalInfoRO;
+import com.scnujxjy.backendpoint.model.vo.PageVO;
+import com.scnujxjy.backendpoint.model.vo.registration_record_card.PersonalInfoVO;
+import com.scnujxjy.backendpoint.service.registration_record_card.PersonalInfoService;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.Objects;
+
+import static com.scnujxjy.backendpoint.exception.DataException.*;
 
 /**
  * <p>
@@ -17,5 +27,86 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/personal-info")
 public class PersonalInfoController {
 
+    @Resource
+    private PersonalInfoService personalInfoService;
+
+    /**
+     * 根据id查询个人基本信息
+     *
+     * @param id 个人基本信息id
+     * @return 个人基本信息
+     */
+    public SaResult detailById(Long id) {
+        // 校验参数
+        if (Objects.isNull(id)) {
+            throw dataMissError();
+        }
+        // 查询数据
+        PersonalInfoVO personalInfoVO = personalInfoService.detailById(id);
+        if (Objects.isNull(personalInfoVO)) {
+            throw dataNotFoundError();
+        }
+        return SaResult.data(personalInfoVO);
+    }
+
+    /**
+     * 分页查询个人基本信息
+     *
+     * @param personalInfoROPageRO 分页查询参数
+     * @return 分页查询结果
+     */
+    public SaResult pageQueryPersonalInfo(PageRO<PersonalInfoRO> personalInfoROPageRO) {
+        // 校验参数
+        if (Objects.isNull(personalInfoROPageRO)) {
+            throw dataMissError();
+        }
+        if (Objects.isNull(personalInfoROPageRO.getEntity())) {
+            personalInfoROPageRO.setEntity(new PersonalInfoRO());
+        }
+        // 查询数据
+        PageVO<PersonalInfoVO> personalInfoVOPageVO = personalInfoService.pageQueryPersonalInfo(personalInfoROPageRO);
+        if (Objects.isNull(personalInfoVOPageVO)) {
+            throw dataNotFoundError();
+        }
+        return SaResult.data(personalInfoROPageRO);
+    }
+
+    /**
+     * 根据id更新个人基本信息
+     *
+     * @param personalInfoRO 个人基本信息
+     * @return 更新后的个人基本信息
+     */
+    public SaResult editById(PersonalInfoRO personalInfoRO) {
+        // 校验参数
+        if (Objects.isNull(personalInfoRO) || Objects.isNull(personalInfoRO.getId())) {
+            throw dataMissError();
+        }
+        // 更新数据
+        PersonalInfoVO personalInfoVO = personalInfoService.editById(personalInfoRO);
+        if (Objects.isNull(personalInfoVO)) {
+            throw dataUpdateError();
+        }
+        return SaResult.data(personalInfoVO);
+    }
+
+    /**
+     * 根据id删除数据
+     *
+     * @param id 个人基本信息id
+     * @return 删除的数据条数
+     */
+    public SaResult deleteById(Long id) {
+        // 校验参数
+        if (Objects.isNull(id)) {
+            throw dataMissError();
+        }
+        // 删除数据
+        int count = personalInfoService.deleteById(id);
+        if (count <= 0) {
+            throw dataDeleteError();
+        }
+        return SaResult.data(count);
+    }
 }
 
--- src/main/java/com/scnujxjy/backendpoint/service/registration_record_card/PersonalInfoService.java
+++ src/main/java/com/scnujxjy/backendpoint/service/registration_record_card/PersonalInfoService.java
@@ -1,10 +1,24 @@
 package com.scnujxjy.backendpoint.service.registration_record_card;
 
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.scnujxjy.backendpoint.dao.entity.registration_record_card.PersonalInfoPO;
 import com.scnujxjy.backendpoint.dao.mapper.registration_record_card.PersonalInfoMapper;
+import com.scnujxjy.backendpoint.inverter.registration_record_card.PersonalInfoInverter;
+import com.scnujxjy.backendpoint.model.ro.PageRO;
+import com.scnujxjy.backendpoint.model.ro.registration_record_card.PersonalInfoRO;
+import com.scnujxjy.backendpoint.model.vo.PageVO;
+import com.scnujxjy.backendpoint.model.vo.registration_record_card.PersonalInfoVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * <p>
@@ -15,6 +29,114 @@ import org.springframework.stereotype.Service;
  * @since 2023-08-04
  */
 @Service
+@Slf4j
 public class PersonalInfoService extends ServiceImpl<PersonalInfoMapper, PersonalInfoPO> implements IService<PersonalInfoPO> {
+    @Resource
+    private PersonalInfoInverter personalInfoInverter;
+
+    /**
+     * 根据id查询个人基本信息
+     *
+     * @param id 个人基本信息id
+     * @return 个人基本信息
+     */
+    public PersonalInfoVO detailById(Long id) {
+        // 校验参数
+        if (Objects.isNull(id)) {
+            log.error("参数缺失");
+            return null;
+        }
+        // 查询数据
+        PersonalInfoPO personalInfoPO = baseMapper.selectById(id);
+        return personalInfoInverter.po2VO(personalInfoPO);
+    }
+
+    /**
+     * 分页查询个人基本信息
+     *
+     * @param personalInfoROPageRO 分页查询参数
+     * @return 分页查询结果
+     */
+    public PageVO<PersonalInfoVO> pageQueryPersonalInfo(PageRO<PersonalInfoRO> personalInfoROPageRO) {
+        // 校验参数
+        if (Objects.isNull(personalInfoROPageRO)) {
+            log.error("参数缺失");
+            return null;
+        }
+        PersonalInfoRO entity = personalInfoROPageRO.getEntity();
+        if (Objects.isNull(entity)) {
+            entity = new PersonalInfoRO();
+        }
+        // 构造查询参数
+        LambdaQueryWrapper<PersonalInfoPO> wrapper = Wrappers.<PersonalInfoPO>lambdaQuery()
+                .eq(Objects.nonNull(entity.getId()), PersonalInfoPO::getId, entity.getId())
+                .eq(StrUtil.isNotBlank(entity.getGender()), PersonalInfoPO::getGender, entity.getGender())
+                .eq(Objects.nonNull(entity.getBirthDate()), PersonalInfoPO::getBirthDate, entity.getBirthDate())
+                .eq(StrUtil.isNotBlank(entity.getPoliticalStatus()), PersonalInfoPO::getPoliticalStatus, entity.getPoliticalStatus())
+                .eq(StrUtil.isNotBlank(entity.getEthnicity()), PersonalInfoPO::getEthnicity, entity.getEthnicity())
+                .eq(StrUtil.isNotBlank(entity.getNativePlace()), PersonalInfoPO::getNativePlace, entity.getNativePlace())
+                .eq(StrUtil.isNotBlank(entity.getIdType()), PersonalInfoPO::getIdType, entity.getIdType())
+                .eq(StrUtil.isNotBlank(entity.getIdNumber()), PersonalInfoPO::getIdNumber, entity.getIdNumber())
+                .eq(StrUtil.isNotBlank(entity.getPostalCode()), PersonalInfoPO::getPostalCode, entity.getPostalCode())
+                .eq(StrUtil.isNotBlank(entity.getPhoneNumber()), PersonalInfoPO::getPhoneNumber, entity.getPhoneNumber())
+                .eq(StrUtil.isNotBlank(entity.getEmail()), PersonalInfoPO::getEmail, entity.getEmail())
+                .eq(StrUtil.isNotBlank(entity.getAddress()), PersonalInfoPO::getAddress, entity.getAddress())
+                .eq(StrUtil.isNotBlank(entity.getIsDisabled()), PersonalInfoPO::getIsDisabled, entity.getIsDisabled())
+                .eq(StrUtil.isNotBlank(entity.getGrade()), PersonalInfoPO::getGrade, entity.getGrade());
+
+        // 查询数据
+        if (Objects.equals(true, personalInfoROPageRO.getIsAll())) {
+            List<PersonalInfoPO> personalInfoPOS = baseMapper.selectList(wrapper);
+            return new PageVO<>(personalInfoInverter.po2VO(personalInfoPOS));
+        } else {
+            Page<PersonalInfoPO> personalInfoPOPage = baseMapper.selectPage(personalInfoROPageRO.getPage(), wrapper);
+            return new PageVO<>(personalInfoPOPage, personalInfoInverter.po2VO(personalInfoPOPage.getRecords()));
+        }
+    }
+
+    /**
+     * 根据id更新个人基本信息
+     *
+     * @param personalInfoRO 个人基本信息
+     * @return 更新后的个人基本信息
+     */
+    public PersonalInfoVO editById(PersonalInfoRO personalInfoRO) {
+        // 校验参数
+        if (Objects.isNull(personalInfoRO) || Objects.isNull(personalInfoRO.getId())) {
+            log.error("参数缺失");
+            return null;
+        }
+        // 更新数据
+        PersonalInfoPO personalInfoPO = personalInfoInverter.ro2PO(personalInfoRO);
+        int count = baseMapper.updateById(personalInfoPO);
+        if (count <= 0) {
+            log.error("更新失败，数据：{}", personalInfoPO);
+            return null;
+        }
+        // 返回更新后的数据
+        return detailById(personalInfoRO.getId());
+    }
+
+    /**
+     * 根据id删除数据
+     *
+     * @param id 个人基本信息id
+     * @return 删除的数据条数
+     */
+    public Integer deleteById(Long id) {
+        // 校验参数
+        if (Objects.isNull(id)) {
+            log.error("参数缺失");
+            return null;
+        }
+        // 删除数据
+        int count = baseMapper.deleteById(id);
+        if (count <= 0) {
+            log.error("删除失败，id：{}", id);
+            return null;
+        }
+        return count;
+    }
+
 
 }
