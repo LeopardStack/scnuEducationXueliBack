aceb8ba7114fc08831a1b4f0fa968238cd1e78e5
liweitang <levitang@126.com>
2023-08-07 21:30:13 +0800

    update:学籍信息CURD完成

--- src/main/java/com/scnujxjy/backendpoint/controller/registration_record_card/StudentStatusController.java
+++ src/main/java/com/scnujxjy/backendpoint/controller/registration_record_card/StudentStatusController.java
@@ -1,9 +1,18 @@
 package com.scnujxjy.backendpoint.controller.registration_record_card;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import cn.dev33.satoken.util.SaResult;
+import com.scnujxjy.backendpoint.model.ro.PageRO;
+import com.scnujxjy.backendpoint.model.ro.registration_record_card.StudentStatusRO;
+import com.scnujxjy.backendpoint.model.vo.PageVO;
+import com.scnujxjy.backendpoint.model.vo.registration_record_card.StudentStatusVO;
+import com.scnujxjy.backendpoint.service.registration_record_card.StudentStatusService;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
+import java.util.Objects;
+
+import static com.scnujxjy.backendpoint.exception.DataException.*;
 
 /**
  * <p>
@@ -17,5 +26,90 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/student-status")
 public class StudentStatusController {
 
+    @Resource
+    private StudentStatusService studentStatusService;
+
+    /**
+     * 根据id查询学籍信息
+     *
+     * @param id 学籍信息id
+     * @return 学籍信息
+     */
+    @GetMapping("/detail")
+    public SaResult detailById(Long id) {
+        // 校验参数
+        if (Objects.isNull(id)) {
+            throw dataMissError();
+        }
+        // 查询
+        StudentStatusVO studentStatusVO = studentStatusService.detailById(id);
+        if (Objects.isNull(studentStatusVO)) {
+            throw dataNotFoundError();
+        }
+        return SaResult.data(studentStatusVO);
+    }
+
+    /**
+     * 分页查询学籍信息
+     *
+     * @param studentStatusROPageRO 分页参数
+     * @return 学籍信息列表
+     */
+    @PostMapping("/page")
+    public SaResult pageQueryStudentStatus(@RequestBody PageRO<StudentStatusRO> studentStatusROPageRO) {
+        // 校验参数
+        if (Objects.isNull(studentStatusROPageRO)) {
+            throw dataMissError();
+        }
+        if (Objects.isNull(studentStatusROPageRO.getEntity())) {
+            studentStatusROPageRO.setEntity(new StudentStatusRO());
+        }
+        // 查询
+        PageVO<StudentStatusVO> studentStatusVOPageVO = studentStatusService.pageQueryStudentStatus(studentStatusROPageRO);
+        if (Objects.isNull(studentStatusVOPageVO)) {
+            throw dataNotFoundError();
+        }
+        return SaResult.data(studentStatusVOPageVO);
+    }
+
+    /**
+     * 更新学籍信息
+     *
+     * @param studentStatusRO 学籍信息
+     * @return 更新后的学籍信息
+     */
+    @PutMapping("/edit")
+    public SaResult editById(StudentStatusRO studentStatusRO) {
+        // 校验参数
+        if (Objects.isNull(studentStatusRO) || Objects.isNull(studentStatusRO.getId())) {
+            throw dataMissError()
+        }
+        // 更新学籍信息
+        StudentStatusVO studentStatusVO = studentStatusService.editById(studentStatusRO);
+        if (Objects.isNull(studentStatusVO)) {
+            throw dataUpdateError();
+        }
+        return SaResult.data(studentStatusVO);
+    }
+
+    /**
+     * 删除学籍信息
+     *
+     * @param id 学籍信息id
+     * @return 删除学籍信息的数量
+     */
+    @DeleteMapping("/delete")
+    public SaResult deleteById(Long id) {
+        // 校验参数
+        if (Objects.isNull(id)) {
+            throw dataDeleteError();
+        }
+        // 删除学籍信息
+        int count = studentStatusService.deleteById(id);
+        if (count <= 0) {
+            throw dataDeleteError();
+        }
+        return SaResult.data(count);
+    }
 }
 
--- src/main/java/com/scnujxjy/backendpoint/service/registration_record_card/StudentStatusService.java
+++ src/main/java/com/scnujxjy/backendpoint/service/registration_record_card/StudentStatusService.java
@@ -1,10 +1,24 @@
 package com.scnujxjy.backendpoint.service.registration_record_card;
 
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.scnujxjy.backendpoint.dao.entity.registration_record_card.StudentStatusPO;
 import com.scnujxjy.backendpoint.dao.mapper.registration_record_card.StudentStatusMapper;
+import com.scnujxjy.backendpoint.inverter.registration_record_card.StudentStatusInverter;
+import com.scnujxjy.backendpoint.model.ro.PageRO;
+import com.scnujxjy.backendpoint.model.ro.registration_record_card.StudentStatusRO;
+import com.scnujxjy.backendpoint.model.vo.PageVO;
+import com.scnujxjy.backendpoint.model.vo.registration_record_card.StudentStatusVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * <p>
@@ -15,6 +29,109 @@ import org.springframework.stereotype.Service;
  * @since 2023-08-04
  */
 @Service
+@Slf4j
 public class StudentStatusService extends ServiceImpl<StudentStatusMapper, StudentStatusPO> implements IService<StudentStatusPO> {
+    @Resource
+    private StudentStatusInverter studentStatusInverter;
 
+    /**
+     * 根据id查询学籍信息
+     *
+     * @param id 学籍信息id
+     * @return 学籍信息
+     */
+    public StudentStatusVO detailById(Long id) {
+        // 校验参数
+        if (Objects.isNull(id)) {
+            log.error("参数缺失");
+            return null;
+        }
+        // 查询
+        StudentStatusPO studentStatusPO = baseMapper.selectById(id);
+        return studentStatusInverter.po2VO(studentStatusPO);
+    }
+
+    /**
+     * 分页查询学籍信息
+     *
+     * @param studentStatusROPageRO 分页参数
+     * @return 学籍信息列表
+     */
+    public PageVO<StudentStatusVO> pageQueryStudentStatus(PageRO<StudentStatusRO> studentStatusROPageRO) {
+        // 校验参数
+        if (Objects.isNull(studentStatusROPageRO)) {
+            log.error("参数缺失");
+            return null;
+        }
+        StudentStatusRO entity = studentStatusROPageRO.getEntity();
+        if (Objects.isNull(entity)) {
+            entity = new StudentStatusRO();
+        }
+        // 构建查询参数
+        LambdaQueryWrapper<StudentStatusPO> wrapper = Wrappers.<StudentStatusPO>lambdaQuery()
+                .eq(Objects.nonNull(entity.getId()), StudentStatusPO::getId, entity.getId())
+                .eq(StrUtil.isNotBlank(entity.getStudentNumber()), StudentStatusPO::getStudentNumber, entity.getStudentNumber())
+                .eq(StrUtil.isNotBlank(entity.getGrade()), StudentStatusPO::getGrade, entity.getGrade())
+                .eq(StrUtil.isNotBlank(entity.getCollege()), StudentStatusPO::getCollege, entity.getCollege())
+                .eq(StrUtil.isNotBlank(entity.getTeachingPoint()), StudentStatusPO::getTeachingPoint, entity.getTeachingPoint())
+                .eq(StrUtil.isNotBlank(entity.getMajorName()), StudentStatusPO::getMajorName, entity.getMajorName())
+                .eq(StrUtil.isNotBlank(entity.getStudyForm()), StudentStatusPO::getStudyForm, entity.getStudyForm())
+                .eq(StrUtil.isNotBlank(entity.getLevel()), StudentStatusPO::getLevel, entity.getLevel())
+                .eq(StrUtil.isNotBlank(entity.getStudyDuration()), StudentStatusPO::getStudyDuration, entity.getStudyDuration())
+                .eq(StrUtil.isNotBlank(entity.getAdmissionNumber()), StudentStatusPO::getAdmissionNumber, entity.getAdmissionNumber())
+                .eq(StrUtil.isNotBlank(entity.getAcademicStatus()), StudentStatusPO::getAcademicStatus, entity.getAcademicStatus())
+                .eq(Objects.nonNull(entity.getEnrollmentDate()), StudentStatusPO::getEnrollmentDate, entity.getEnrollmentDate());
+        // 列表查询 或 分页查询 并返回结果
+        if (Objects.equals(true, studentStatusROPageRO.getIsAll())) {
+            List<StudentStatusPO> studentStatusPOS = baseMapper.selectList(wrapper);
+            return new PageVO<>(studentStatusInverter.po2VO(studentStatusPOS));
+        } else {
+            Page<StudentStatusPO> studentStatusPOPage = baseMapper.selectPage(studentStatusROPageRO.getPage(), wrapper);
+            return new PageVO<>(studentStatusPOPage, studentStatusInverter.po2VO(studentStatusPOPage.getRecords()));
+        }
+    }
+
+    /**
+     * 更新学籍信息
+     *
+     * @param studentStatusRO 学籍信息
+     * @return 更新后的学籍信息
+     */
+    public StudentStatusVO editById(StudentStatusRO studentStatusRO) {
+        // 校验参数
+        if (Objects.isNull(studentStatusRO) || Objects.isNull(studentStatusRO.getId())) {
+            log.error("参数缺失");
+            return null;
+        }
+        // 更新学籍信息
+        StudentStatusPO studentStatusPO = studentStatusInverter.ro2PO(studentStatusRO);
+        int count = baseMapper.updateById(studentStatusPO);
+        if (count <= 0) {
+            log.error("更新失败，数据：{}", studentStatusPO);
+            return null;
+        }
+
+        return detailById(studentStatusRO.getId());
+    }
+
+    /**
+     * 删除学籍信息
+     *
+     * @param id 学籍信息id
+     * @return 删除学籍信息的数量
+     */
+    public Integer deleteById(Long id) {
+        // 校验参数
+        if (Objects.isNull(id)) {
+            log.error("参数缺失");
+            return null;
+        }
+        // 删除学籍信息
+        int count = baseMapper.deleteById(id);
+        if (count <= 0) {
+            log.error("删除失败，id：{}", id);
+            return null;
+        }
+        return count;
+    }
 }
