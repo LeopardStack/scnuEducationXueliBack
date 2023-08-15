0137f1d1eccfe46e2407063186f5a9ebbd934c89
liweitang <levitang@126.com>
2023-08-15 23:28:47 +0800

    update:课程信息CURD完成

--- src/main/java/com/scnujxjy/backendpoint/controller/teaching_process/CourseInformationController.java
+++ src/main/java/com/scnujxjy/backendpoint/controller/teaching_process/CourseInformationController.java
@@ -1,8 +1,18 @@
 package com.scnujxjy.backendpoint.controller.teaching_process;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cn.dev33.satoken.util.SaResult;
+import com.scnujxjy.backendpoint.model.ro.PageRO;
+import com.scnujxjy.backendpoint.model.ro.teaching_process.CourseInformationRO;
+import com.scnujxjy.backendpoint.model.vo.PageVO;
+import com.scnujxjy.backendpoint.model.vo.teaching_process.CourseInformationVO;
+import com.scnujxjy.backendpoint.service.teaching_process.CourseInformationService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.Objects;
+
+import static com.scnujxjy.backendpoint.exception.DataException.*;
 
 /**
  * <p>
@@ -15,6 +25,98 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/course-information")
 public class CourseInformationController {
+    @Resource
+    private CourseInformationService courseInformationService;
 
+    /**
+     * 根据id查询课程信息
+     *
+     * @param id 课程信息id
+     * @return 课程信息
+     */
+    @GetMapping("/detail")
+    public SaResult detailById(Long id) {
+        // 参数校验
+        if (Objects.isNull(id)) {
+            throw dataMissError();
+        }
+        // 查询
+        CourseInformationVO courseInformationVO = courseInformationService.detailById(id);
+        // 参数校验
+        if (Objects.isNull(courseInformationVO)) {
+            throw dataNotFoundError();
+        }
+        // 转换并返回
+        return SaResult.data(courseInformationVO);
+    }
+
+    /**
+     * 分页查询课程信息
+     *
+     * @param courseInformationROPageRO 分页参数
+     * @return 分页查询的课程信息列表
+     */
+    @PostMapping("/page")
+    public SaResult pageQueryCourseInformation(@RequestBody PageRO<CourseInformationRO> courseInformationROPageRO) {
+        // 参数校验
+        if (Objects.isNull(courseInformationROPageRO)) {
+            throw dataMissError();
+        }
+        if (Objects.isNull(courseInformationROPageRO.getEntity())) {
+            courseInformationROPageRO.setEntity(new CourseInformationRO());
+        }
+        // 查询数据
+        PageVO<CourseInformationVO> courseInformationVOPageVO = courseInformationService.pageQueryCourseInformation(courseInformationROPageRO);
+        // 数据校验
+        if (Objects.isNull(courseInformationVOPageVO)) {
+            throw dataNotFoundError();
+        }
+        // 返回数据
+        return SaResult.data(courseInformationVOPageVO);
+    }
+
+    /**
+     * 根据id更新课程信息
+     *
+     * @param courseInformationRO 课程信息
+     * @return 更新后的课程信息
+     */
+    @PutMapping("/edit")
+    public SaResult editById(@RequestBody CourseInformationRO courseInformationRO) {
+        // 参数校验
+        if (Objects.isNull(courseInformationRO) || Objects.nonNull(courseInformationRO.getId())) {
+            throw dataMissError();
+        }
+        // 更新
+        CourseInformationVO courseInformationVO = courseInformationService.editById(courseInformationRO);
+        // 校验数据
+        if (Objects.isNull(courseInformationVO)) {
+            throw dataUpdateError();
+        }
+        // 返回数据
+        return SaResult.data(courseInformationVO);
+    }
+
+    /**
+     * 根据id删除课程信息
+     *
+     * @param id 课程信息id
+     * @return 删除数量
+     */
+    @DeleteMapping("/delete")
+    public SaResult deleteById(Long id) {
+        // 参数校验
+        if (Objects.isNull(id)) {
+            throw dataMissError();
+        }
+        // 删除数据
+        int count = courseInformationService.deleteById(id);
+        // 校验数据
+        if (count <= 0) {
+            throw dataDeleteError();
+        }
+        // 返回数据
+        return SaResult.data(count);
+    }
 }
 
---
+++ src/main/java/com/scnujxjy/backendpoint/model/ro/teaching_process/CourseInformationRO.java
@@ -0,0 +1,91 @@
+package com.scnujxjy.backendpoint.model.ro.teaching_process;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 课程信息表
+ * </p>
+ *
+ * @author leopard
+ * @since 2023-08-14
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Accessors(chain = true)
+public class CourseInformationRO {
+
+
+    /**
+     * 自增主键
+     */
+    private Long id;
+
+    /**
+     * 年级
+     */
+    private String grade;
+
+    /**
+     * 专业名称
+     */
+    private String majorName;
+
+    /**
+     * 层次
+     */
+    private String level;
+
+    /**
+     * 学习形式
+     */
+    private String studyForm;
+
+    /**
+     * 行政班别
+     */
+    private String adminClass;
+
+    /**
+     * 课程名称
+     */
+    private String courseName;
+
+    /**
+     * 学时数
+     */
+    private Integer studyHours;
+
+    /**
+     * 考核类型
+     */
+    private String assessmentType;
+
+    /**
+     * 授课方式
+     */
+    private String teachingMethod;
+
+    /**
+     * 课程类型
+     */
+    private String courseType;
+
+    /**
+     * 学分
+     */
+    private Integer credit;
+
+    /**
+     * 授课学期
+     */
+    private String teachingSemester;
+
+
+}
