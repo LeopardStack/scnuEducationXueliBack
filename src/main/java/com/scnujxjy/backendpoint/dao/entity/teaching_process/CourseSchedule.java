cdee3781c412052db2b222af3b01c7b0f92c8559
leopard <2904083750@qq.com>
2023-08-20 15:11:00 +0800

    update:生成排课表代码

---
+++ src/main/java/com/scnujxjy/backendpoint/dao/entity/teaching_process/CourseSchedule.java
@@ -0,0 +1,140 @@
+package com.scnujxjy.backendpoint.dao.entity.teaching_process;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import java.util.Date;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.Version;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * <p>
+ * 排课表
+ * </p>
+ *
+ * @author leopard
+ * @since 2023-08-18
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName("course_schedule")
+public class CourseSchedule implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 自增主键
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 年级
+     */
+    private String grade;
+
+    /**
+     * 专业名称
+     */
+    private String majorName;
+
+    /**
+     * 层次
+     */
+    private String level;
+
+    /**
+     * 学习形式
+     */
+    private String studyForm;
+
+    /**
+     * 行政班别
+     */
+    private String adminClass;
+
+    /**
+     * 教学班别
+     */
+    private String teachingClass;
+
+    /**
+     * 学生人数
+     */
+    private Integer studentCount;
+
+    /**
+     * 课程名称
+     */
+    private String courseName;
+
+    /**
+     * 学时数
+     */
+    private Integer classHours;
+
+    /**
+     * 考核类型
+     */
+    private String examType;
+
+    /**
+     * 主讲教师
+     */
+    private String mainTeacherName;
+
+    /**
+     * 主讲教师工号/学号
+     */
+    private String mainTeacherId;
+
+    /**
+     * 主讲教师身份证号码
+     */
+    private String mainTeacherIdentity;
+
+    /**
+     * 辅导教师
+     */
+    private String tutorName;
+
+    /**
+     * 辅导教师工号
+     */
+    private String tutorId;
+
+    /**
+     * 辅导教师身份证号码
+     */
+    private String tutorIdentity;
+
+    /**
+     * 授课方式（直播、点播、线下）
+     */
+    private String teachingMethod;
+
+    /**
+     * 上课地点
+     */
+    private String classLocation;
+
+    /**
+     * 在线教学平台及网络课程资源拥有情况
+     */
+    private String onlinePlatform;
+
+    /**
+     * 授课日期
+     */
+    private Date teachingDate;
+
+    /**
+     * 授课时间
+     */
+    private String teachingTime;
+
+
+}
---
+++ src/main/java/com/scnujxjy/backendpoint/dao/mapper/teaching_process/CourseScheduleMapper.java
@@ -0,0 +1,16 @@
+package com.scnujxjy.backendpoint.dao.mapper.teaching_process;
+
+import com.scnujxjy.backendpoint.dao.entity.teaching_process.CourseSchedule;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 排课表 Mapper 接口
+ * </p>
+ *
+ * @author leopard
+ * @since 2023-08-18
+ */
+public interface CourseScheduleMapper extends BaseMapper<CourseSchedule> {
+
+}
