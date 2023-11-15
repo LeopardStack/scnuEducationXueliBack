2a6ce3fdf24aa9ea688bfc5df28f7ddfac3eb4b2
leopard <2904083750@qq.com>
2023-11-15 17:10:51 +0800

    删除多余的转专业申请表单

--- src/main/java/com/scnujxjy/backendpoint/dao/mongoEntity/StudentTransferApplicationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.scnujxjy.backendpoint.dao.mongoEntity;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StudentTransferApplicationRepository extends MongoRepository<StudentTransferApplication, String> {
-    // 这里可以添加一些自定义的查询方法，如果需要的话
-}
