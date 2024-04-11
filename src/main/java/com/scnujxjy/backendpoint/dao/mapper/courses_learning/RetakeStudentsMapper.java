2f1a0010a42d23d781403554984821a5b84cb595
litong <19802021990@139.com>
2024-04-11 23:54:38 +0800

    更新白名单得加重修学生

--- src/main/java/com/scnujxjy/backendpoint/dao/mapper/courses_learning/RetakeStudentsMapper.java
+++ src/main/java/com/scnujxjy/backendpoint/dao/mapper/courses_learning/RetakeStudentsMapper.java
@@ -2,6 +2,9 @@ package com.scnujxjy.backendpoint.dao.mapper.courses_learning;
 
 import com.scnujxjy.backendpoint.dao.entity.courses_learning.RetakeStudentsPO;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 public interface RetakeStudentsMapper extends BaseMapper<RetakeStudentsPO> {
 
+    @Select("select * from retake_students where course_id=#{courseId}")
+    List<RetakeStudentsPO> selectByCourseId(Long courseId);
 }
